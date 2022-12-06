5026d9847f39562b8ee92d8dfb7a4620e720c7d5
SatooRu65536 <ryuto4619@gmail.com>
2022-12-07 01:36:00 +0900

    :art: main.dart 分割

--- lib/main.dart
+++ lib/main.dart
@@ -9,64 +9,71 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Theme.of(context).primaryColor,
-          notchMargin: 6.0,
-          shape: const AutomaticNotchedShape(
-            RoundedRectangleBorder(),
-            StadiumBorder(
-              side: BorderSide(),
-            ),
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomeView());
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.map),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).primaryColor,
+        notchMargin: 6.0,
+        shape: const AutomaticNotchedShape(
+          RoundedRectangleBorder(),
+          StadiumBorder(
+            side: BorderSide(),
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                const Spacer(flex: 2),
-                IconButton(
-                  icon: const Icon(
-                    Icons.home_filled,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              const Spacer(flex: 2),
+              IconButton(
+                icon: const Icon(
+                  Icons.home_filled,
+                  color: Colors.white,
                 ),
-                const Spacer(flex: 2),
-                IconButton(
-                  icon: const Icon(
-                    Icons.add_box,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
+                onPressed: () {},
+              ),
+              const Spacer(flex: 2),
+              IconButton(
+                icon: const Icon(
+                  Icons.add_box,
+                  color: Colors.white,
                 ),
-                const SizedBox(width: 100),
-                IconButton(
-                  icon: const Icon(
-                    Icons.mail,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
+                onPressed: () {},
+              ),
+              const SizedBox(width: 100),
+              IconButton(
+                icon: const Icon(
+                  Icons.mail,
+                  color: Colors.white,
                 ),
-                const Spacer(flex: 2),
-                IconButton(
-                  icon: const Icon(
-                    Icons.settings,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
+                onPressed: () {},
+              ),
+              const Spacer(flex: 2),
+              IconButton(
+                icon: const Icon(
+                  Icons.settings,
+                  color: Colors.white,
                 ),
-                const Spacer(flex: 2),
-              ],
-            ),
+                onPressed: () {},
+              ),
+              const Spacer(flex: 2),
+            ],
           ),
         ),
       ),
