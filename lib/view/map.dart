17d78384e9a50985bc17b87732ca1092230d0f96
SatooRu65536 <ryuto4619@gmail.com>
2023-01-09 23:32:58 +0900

    :sparkles: マップの表示できた

--- lib/view/map.dart
+++ lib/view/map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapView extends StatelessWidget {
   const MapView({super.key});
@@ -6,7 +8,25 @@ class MapView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(child: Text('Map', style: TextStyle(fontSize: 32.0))),
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(35.08319, 136.98794),
+          zoom: 9.2,
+        ),
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: 'OpenStreetMap contributors',
+            onSourceTapped: null,
+          ),
+        ],
+        children: [
+          TileLayer(
+            urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            subdomains: const ['a', 'b', 'c'],
+            retinaMode: true,
+          ),
+        ],
+      ),
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
     );
   }
